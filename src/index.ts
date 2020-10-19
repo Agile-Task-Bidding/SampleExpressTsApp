05fdfbaee8805cbedfbc605d9240ce981cfb18b7
Ryan <ryan.cfplus@gmail.com>
2020-10-19 14:05:16 -0400

    2nd Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const express = require( "express" );
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Read environment variables
+dotenv.config();
+
+// Grab port
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+
+const app = express();
+
+// This will be useful for POST requests
+app.use(bodyParser.json());
+
+// Handle api requests
+app.get("/api/*", (req: any, res: any) => {
+    res.send(JSON.stringify({data: 'alive', error: ''}));
+});
+
+// Serve the compiled React app for any other path
+app.get( "*", ( req: any, res: any ) => {
+    res.sendFile(path.join(__dirname, 'res/index.html'));
+});
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+});
