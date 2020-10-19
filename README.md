e5b8d9db829426db3432ed26891598619c73c70b
Ryan <ryan.cfplus@gmail.com>
2020-10-19 14:26:24 -0400

    Fixes index.html location and adds Readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sample Express TypeScript App
+
+Experimenting with Express and TypeScript
+
+Install dependencies:
+```bash
+npm init
+```
+Add an index.html file to `/build/public/`. A sample is provided here:
+```html
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Yeet</h1>
+    <p>Cool</p>
+  </body>
+</html>
+```
+Compile and run:
+```bash
+npm run build
+npm start
+```
--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,17 @@ const app = express();
 // This will be useful for POST requests
 app.use(bodyParser.json());
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Handle api requests
 app.get("/api/*", (req: any, res: any) => {
     res.send(JSON.stringify({data: 'alive', error: ''}));
 });
 
-// Serve the compiled React app for any other path
+// Serve the primary React app for any other path
 app.get( "*", ( req: any, res: any ) => {
-    res.sendFile(path.join(__dirname, 'res/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // start the Express server
